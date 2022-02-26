431302d7c4dbae273b26bd603d3d4ef12c7ae3a0
Jean Loberg Hansen <jeanloberg@gmail.com>
2022-02-26 17:53:14 +0100

    finsih 404 page

--- types/layout/globalData.d.ts
+++ types/layout/globalData.d.ts
@@ -1,5 +1,7 @@
 declare namespace NGlobalData {
   export interface IGlobalData extends NFooter.IFooterData {
     headerLogo: string;
+    pageNotFoundTitle?: string;
+    pageNotFoundBodytext?: string;
   }
 }
