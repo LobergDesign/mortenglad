f2c21c8fb4016aed8ed06adec237e14496ccbd63
Jean Loberg Hansen <jeanloberg@gmail.com>
2022-03-06 18:12:12 +0100

    fix cvcollections issues

--- components/cvCollections/cvCollectionNestedItem/cvCollectionNestedItem.vue
+++ components/cvCollections/cvCollectionNestedItem/cvCollectionNestedItem.vue
@@ -2,56 +2,11 @@
   <ul
     class="grid-r reset-ul cv-collection-nested-list last-text-align-right reset-align-xs"
   >
-    <template v-if="len === 4">
+    <template v-if="len <= 4">
       <li class="grid-c-xs-6 grid-c-3">
         <span class="cv-collection-nested-list__item-headline mobile-only">
           {{ headlines[0] }}
         </span>
-        <span class="cv-collection-nested-list__item">
-          <template v-if="data.type">
-            {{ data.type }}
-          </template>
-          <template v-else-if="data.educationcourse">
-            {{ data.educationcourse }}
-          </template>
-        </span>
-      </li>
-      <li class="grid-c-xs-6 grid-c-5">
-        <span class="cv-collection-nested-list__item-headline mobile-only">
-          {{ headlines[1] }}
-        </span>
-        <span class="cv-collection-nested-list__item">
-          <template v-if="data.production">
-            {{ data.production }}
-          </template>
-          <template v-else-if="data.place">
-            {{ data.place }}
-          </template>
-        </span>
-      </li>
-      <li class="grid-c-xs-6 grid-c-3">
-        <span class="cv-collection-nested-list__item-headline mobile-only">
-          {{ headlines[2] }}
-        </span>
-        <span class="cv-collection-nested-list__item">
-          {{ data.role }}
-        </span>
-      </li>
-      <li class="grid-c-xs-6 grid-c-1">
-        <span class="cv-collection-nested-list__item-headline mobile-only">
-          {{ headlines[3] }}
-        </span>
-        <span class="cv-collection-nested-list__item">
-          {{ data.year }}
-        </span>
-      </li>
-    </template>
-    <template v-if="len === 3">
-      <li class="grid-c-xs-6 grid-c-5">
-        <span class="cv-collection-nested-list__item-headline mobile-only">
-          {{ headlines[0] }}
-        </span>
-
         <span class="cv-collection-nested-list__item">
           <template v-if="data.type">
             {{ data.type }}
@@ -64,7 +19,7 @@
           </template>
         </span>
       </li>
-      <li class="grid-c-xs-6 grid-c-6">
+      <li class="grid-c-xs-6 grid-c-5">
         <span class="cv-collection-nested-list__item-headline mobile-only">
           {{ headlines[1] }}
         </span>
@@ -77,9 +32,22 @@
           </template>
         </span>
       </li>
-      <li class="grid-c-xs-6 grid-c-1">
+      <li class="grid-c-xs-6 grid-c-3">
         <span class="cv-collection-nested-list__item-headline mobile-only">
           {{ headlines[2] }}
+        </span>
+        <span class="cv-collection-nested-list__item">
+          <template v-if="data.role">
+            {{ data.role }}
+          </template>
+          <template v-else-if="data.city">
+            {{ data.city }}
+          </template>
+        </span>
+      </li>
+      <li class="grid-c-xs-6 grid-c-1">
+        <span class="cv-collection-nested-list__item-headline mobile-only">
+          {{ headlines[3] }}
         </span>
         <span class="cv-collection-nested-list__item">
           {{ data.year }}
