c8f8d0b51f91670161bda3ab2c8a0bf3c7706394
Jean Loberg Hansen <jeanloberg@gmail.com>
2022-03-20 11:49:30 +0100

    finish gallery and frontpage hero responsive

--- components/imageGallery/imageGallery.vue
+++ components/imageGallery/imageGallery.vue
@@ -12,44 +12,48 @@
         </div>
       </li>
     </ul>
-
-    <div v-if="isActive" class="image-gallery-enlarged">
-      <button
-        type="button"
-        class="image-gallery-enlarged__close"
-        @click="closeGallery"
-      >
-        <Cross />
-      </button>
-      <div class="image-gallery-enlarged__backdrop" @click="closeGallery"></div>
-      <button
-        type="button"
-        class="image-gallery-enlarged__btn"
-        :class="{ 'is-inactive': !less }"
-        @click="prev"
-      >
-        <Arrow />
-      </button>
-      <ul ref="gallery" class="image-gallery-enlarged__list">
-        <li v-for="(item, i) in images" :key="i">
-          <div class="image" :class="{ 'is-active': activeIndex === i }">
-            <nuxt-img
-              v-if="item.original_secure_url"
-              provider="cloudinary"
-              :src="item.original_secure_url"
-            />
-          </div>
-        </li>
-      </ul>
-      <button
-        type="button"
-        class="image-gallery-enlarged__btn image-gallery-enlarged__btn--next"
-        :class="{ 'is-inactive': !more }"
-        @click="next"
-      >
-        <Arrow />
-      </button>
-    </div>
+    <transition name="fade">
+      <div v-show="isActive" class="image-gallery-enlarged">
+        <button
+          type="button"
+          class="image-gallery-enlarged__close"
+          @click="closeGallery"
+        >
+          <Cross />
+        </button>
+        <div
+          class="image-gallery-enlarged__backdrop"
+          @click="closeGallery"
+        ></div>
+        <button
+          type="button"
+          class="image-gallery-enlarged__btn"
+          :class="{ 'is-inactive': !less }"
+          @click="prev"
+        >
+          <Arrow />
+        </button>
+        <ul ref="gallery" class="image-gallery-enlarged__list">
+          <li v-for="(item, i) in images" :key="i">
+            <div class="image" :class="{ 'is-active': activeIndex === i }">
+              <nuxt-img
+                v-if="item.original_secure_url"
+                provider="cloudinary"
+                :src="item.original_secure_url"
+              />
+            </div>
+          </li>
+        </ul>
+        <button
+          type="button"
+          class="image-gallery-enlarged__btn image-gallery-enlarged__btn--next"
+          :class="{ 'is-inactive': !more }"
+          @click="next"
+        >
+          <Arrow />
+        </button>
+      </div>
+    </transition>
   </div>
 </template>
 <script lang="ts">
@@ -89,10 +93,11 @@ export default Vue.extend({
     },
     // set active slide index
     setActiveIndex(i: number) {
+      this.isActive = true;
       this.activeIndex = i;
       this.slideGallery(false);
+
       this.moreLess();
-      this.isActive = true;
     },
     // slide gallery
     slideGallery(animate: boolean = true) {
