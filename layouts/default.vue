a6a08600dedd9e4aabf504c82496a7157d1dfe00
Jean Loberg Hansen <jeanloberg@gmail.com>
2022-03-21 21:53:19 +0100

    work on init transition

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="app">
+    <transition appear :css="false" @appear="customBeforeAppear">
+      <div class="app-init-effect">
+        <span class="app-init-effect__text-wrap">
+          <span class="app-init-effect__text" data-init-text-first> Actor</span>
+        </span>
+        <span class="app-init-effect__text-wrap">
+          <span class="app-init-effect__text"> Morten</span></span
+        >
+        <span class="app-init-effect__text-wrap">
+          <span class="app-init-effect__text"> Glad</span>
+        </span>
+      </div>
+    </transition>
     <site-header v-if="!$fetchState.pending" :data="header" />
     <main>
       <Nuxt />
     </main>
-
     <site-footer v-if="!$fetchState.pending" :data="footer" />
   </div>
 </template>
@@ -17,6 +29,7 @@ export default Vue.extend({
     return {
       header: {},
       footer: {},
+      ease: "power4.out",
     };
   },
 
@@ -44,6 +57,40 @@ export default Vue.extend({
       this.header = header;
       this.footer = footer;
     }
+  },
+  methods: {
+    customBeforeAppear() {
+      const SplitText = this.$SplitText;
+      const gsap = this.$gsap as NLib.IGsap;
+      const target = document.querySelector(
+        "[data-init-text-first]"
+      ) as HTMLElement;
+
+      const tl = gsap.timeline();
+      // @ts-ignore
+      const mySplitText = new SplitText(target, { type: "chars" });
+      const chars = mySplitText.chars;
+
+      tl.fromTo(
+        chars,
+        { opacity: 0, x: -80, fontWeight: 100 },
+        {
+          fontWeight: 400,
+          x: 0,
+          opacity: 1,
+          duration: 1.1,
+          stagger: 0.05,
+          ease: this.ease,
+        }
+      ).to(chars, {
+        fontWeight: 100,
+        x: 80,
+        opacity: 0,
+        duration: 1.2,
+        stagger: -0.05,
+        ease: this.ease,
+      });
+    },
   },
 });
 </script>
